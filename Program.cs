7f485805f78f88b9c7d81d1657047924d528ce8c
Jessica <jpanndora@gmail.com>
2025-11-08 00:56:10 -0300

    Atividade Concluida

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("--- Decomposição Decimal ---\n");
+
+Console.Write("Digite um numero inteiro positivo: ");
+int numero = int.Parse(Console.ReadLine()!);
+int unidade = numero % 10;
+int dezena = (numero / 10) % 10;
+int centena = (numero / 100) % 10;
+int milhar = (numero / 1000) % 10;
+Console.WriteLine($"\nUnidade: {unidade}");
+Console.WriteLine($"Dezena: {dezena}");
+Console.WriteLine($"Centena: {centena}");
+Console.WriteLine($"Milhar: {milhar}");
+Console.WriteLine("\n---------------------------");
